9a6f80016702850213235fca485e0d82979a23bb
Alexandre Nascimento <alexandre.n@outlook.com>
2024-12-08 20:52:52 -0300

    Update resource group reference in PostgreSQL server
    Changed the `resource_group_name` in the `azurerm_postgresql_flexible_server` resource to reference the variable `var.resource_group_name` instead of deriving it from the `azurerm_resource_group.rg.name`. This improves flexibility by allowing the resource group to be specified directly through a variable.

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ variable "database_name" {
 resource "azurerm_postgresql_flexible_server" "postgresql_flexible" {
   name                   = var.postgresql_server_name
   location               = var.location
-  resource_group_name    = azurerm_resource_group.rg.name
+  resource_group_name    = var.resource_group_name
   administrator_login    = var.admin_username
   administrator_password = var.admin_password
   sku_name               = "B_Standard_B1ms"
